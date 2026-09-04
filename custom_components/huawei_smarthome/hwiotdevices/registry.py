13830674bc2faa9c6966ba01b4cf2eb7cf44a803
K <129178072+xiasi0@users.noreply.github.com>
2026-09-04 18:46:35 +0800

    feat: add 2F6R light support

--- custom_components/huawei_smarthome/hwiotdevices/registry.py
+++ custom_components/huawei_smarthome/hwiotdevices/registry.py
@@ -12,6 +12,7 @@ from ..mqtt_client import HuaweiMqttClient
 _SUPPORTED_PRODUCTS = {
     "100z": import_module(".lights.100z", package=__package__).HuaweiDevice,
     "20hz": import_module(".lights.20HZ", package=__package__).HuaweiDevice,
+    "2f6r": import_module(".lights.2F6R", package=__package__).HuaweiDevice,
 }
 
 
